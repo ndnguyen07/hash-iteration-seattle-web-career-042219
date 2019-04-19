2e605ebff9c34c45bfd61269c3d8fc5880af0777
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-19 03:09:22 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,12 +1,12 @@
- birthday_kids = {
-   "Timmy" => 9, 
-   "Sarah" => 6, 
-   "Amanda" => 27
- }
+# birthday_kids = {
+#   "Timmy" => 9, 
+#   "Sarah" => 6, 
+#   "Amanda" => 27
+# }
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old"
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
 
